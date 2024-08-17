3a7a11a8ae7ad05ea24617d55d00176776e92efd
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2024-08-17 01:43:09 -0700

    commit changes

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -1,29 +1,49 @@
 let  submitBtn = document.querySelector(".submit-btn")
+let  useridSubmitBtn = document.querySelector(".user_id-submit-btn")
+let  overlay = document.querySelector(".overlay")
 let btnSpan = document.querySelector(".submit-btn > span")
-console.log(btnSpan)
+let useridBtnSpan = document.querySelector(".user_id-submit-btn > span")
 let createInput = document.querySelectorAll(".create-acct-input")
-function emptyValueLength(){
+let useridInput = document.querySelectorAll(".user_id-input")
+function emptyValueLength(el_input){
     let value;
-    createInput.forEach(el => {
+    el_input.forEach(el => {
         if(el.value.length == 0){
             value = true
         }else {
             value = false
         }
+       
     })
+//the input nodelist will be empty if we are on another page, that why with this condition we set value to false to enable the animation to work
+    // if(el_input.length == 0){
+    //     value = false
+    // }
     
     return value
 }
-submitBtn.addEventListener("click", (e) => {
-    let emptyValue = emptyValueLength()
-    console.log(emptyValue)
+
+function startLoadingAnim(el_input, el = "", btnSpan = ""){
+    let emptyValue = emptyValueLength(el_input)
     if (emptyValue == false){
-        e.target.classList.add("active")
+        el.classList.add("active")
 
         setTimeout(() => {
-            e.target.classList.remove("active")
+            el.classList.remove("active")
             btnSpan.textContent = "Successful"
         }, 4000)
     }
-  
-})
+}
+
+if (submitBtn){
+    submitBtn.addEventListener("click", (e) => startLoadingAnim(createInput, e.target, btnSpan))
+}
+
+
+if (useridSubmitBtn){
+    useridSubmitBtn.addEventListener("click", () =>{
+        console.log("hi")
+        
+        startLoadingAnim(useridInput, overlay, useridBtnSpan)
+    })
+}
