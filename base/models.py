c3243164d2535aa98a8b1896a165bba195a983d2
Buchi77344 <onyebuchifestus77@gmail.com>
2024-08-20 22:28:38 +0100

    dijcc

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 
 
-# Create your models here.
+
 class User(AbstractUser):
    school_name =models.CharField(max_length=500,null=True)
    recovery_code = models.CharField(max_length=100,null=True)
@@ -18,7 +18,7 @@ class Question(models.Model):
     text = models.TextField()
     diagram = models.ImageField(upload_to='questions/diagrams/', blank=True, null=True) 
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='questions',null=True)
- # Stores the path to the diagram image
+
 
 
     def __str__(self):
@@ -32,12 +32,6 @@ class Answer(models.Model):
     def __str__(self):
         return self.text
    
-
-
-    
-
-
-
 
 class UserID(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
